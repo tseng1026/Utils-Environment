a64f019776f6eff036aef71ddbd715533424576f
Scarlett Tseng <user@TSENG-MacBook.local>
2020-07-28 17:05:15 +0800

    Application

---
+++ installs.sh
@@ -0,0 +1,18 @@
+brew install wget
+brew install tmux
+brew install zplug
+brew install autojump
+brew install python3
+brew install pip3
+brew cask install docker	# docker pull <image>
+brew cask install vmware-fusion
+brew cask install google-chrome
+brew cask install google-drive-file-stream
+brew cask install sublime-text	# BracketHighlighter, SidebarEnhancements
+brew cask install macdown
+brew cask install mactex
+brew cask install texmaker
+brew cask install slack
+brew cask install spotify
+brew cask install appcleaner
+brew cask update
